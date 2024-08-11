dc0bbda18204536add56795aa51175d9aa02297e
Mike <khushvakov92@gmail.com>
2024-08-11 14:13:17 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");  
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 //1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //2 Sessions code
 
@@ -16,16 +27,59 @@ app.set("view engine", "ejs");
 
 
 //4 Routing code
-app.get("/hello", function(req, res) {
-    res.end("<h1>HELLO WORLD<h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({ test: "success" });
 });
 
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovg'alar bolimidasiz<h1>");
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+app.get("/", function (req, res) {
+    res.render("harid");
 });
+
+
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running succesfully on port: ${PORT}`);
 });
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// // Middleware to parse incoming requests with JSON payloads
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // POST route to handle form submission
+// app.post("/create-item", (req, res) => {
+//     console.log(req.body);
+//     res.json({ test: "success" });
+// });
+
+// // GET route to render the "harid" page
+// app.get("/", (req, res) => {
+//     res.render("harid");
+// });
+
+// // Setting up the view engine (assuming you're using EJS)
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
