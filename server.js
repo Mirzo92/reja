145fecd6f1b11d0ea42c12341261b9aa05ce2e25
Mike <khushvakov92@gmail.com>
2024-08-18 14:27:51 +0900

    fixt:refactor

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/author", (req, res) => {
 })
 
 app.get("/", function (req, res) {
-    res.render("harid");
+    res.render("reja");
 });
 
 
@@ -45,7 +45,7 @@ app.get("/", function (req, res) {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
-    console.log(`The server is running succesfully on port: ${PORT}`);
+    console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
 });
 
 
