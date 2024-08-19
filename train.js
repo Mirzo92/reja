9bf564d0cc51486d67467b1a96117d886da913ac
Mike <khushvakov92@gmail.com>
2024-08-19 20:56:30 +0900

    feat: train.js a task

--- train.js
+++ train.js
@@ -1,31 +1,54 @@
+// A-TASK
 
-console.log('Jack Ma maslaxatlari');
-
-const list = [
-    "yaxshi talaba bo'ling", //  0-20
-    "togri boshliq tanlang va ko'proq xato qiling", //  20-30
-    "uzingizga ishlashni boshlang",  //  30-40
-    "siz kuchli bolgan narsalarni qiling" , //    40-50
-    "yosshlarga investetsiya qiling",   // 50-60
-    "endi dam oling, foydasi yoq",   // 60
-];
-
-async function maslaxatBering(a) {
-    if (typeof a !== "number") throw new Error("insert a number", null);
-    else if (a <= 20) return list[0];
-    else if (a < 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-             resolve(list[5]);
-            }, 5000);   
-        })
-         
+function atask(e, soz) {
+    let count = 0;
+  
+    for (let i = 0; i < soz.length; i++) {
+      if (soz[i] === e) {
+        console.log("before", count);
+        count++;
+      }
     }
-}
+    console.log("after", count);
+    return count;
+  }
+  
+  console.log(atask("e", "engineer"));
+
+
+
+
+
+
+
+
+// console.log('Jack Ma maslaxatlari');
+
+// const list = [
+//     "yaxshi talaba bo'ling", //  0-20
+//     "togri boshliq tanlang va ko'proq xato qiling", //  20-30
+//     "uzingizga ishlashni boshlang",  //  30-40
+//     "siz kuchli bolgan narsalarni qiling" , //    40-50
+//     "yosshlarga investetsiya qiling",   // 50-60
+//     "endi dam oling, foydasi yoq",   // 60
+// ];
+
+// async function maslaxatBering(a) {
+//     if (typeof a !== "number") throw new Error("insert a number", null);
+//     else if (a <= 20) return list[0];
+//     else if (a < 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//              resolve(list[5]);
+//             }, 5000);   
+//         })
+         
+//     }
+// }
 
 // // then, catch
 // console.log("passed here 0");
@@ -38,17 +61,17 @@ async function maslaxatBering(a) {
 // });
 // console.log("passed here 1");
 
-//async await
-async function run() {
-    let javob = await maslaxatBering(25);
-    console.log(javob);
-    // javob = await maslaxatBering(70);
-    // console.log(javob);
-    // javob = await maslaxatBering(41);
-    // console.log(javob);
+// //async await
+// async function run() {
+//     let javob = await maslaxatBering(25);
+//     console.log(javob);
+//     // javob = await maslaxatBering(70);
+//     // console.log(javob);
+//     // javob = await maslaxatBering(41);
+//     // console.log(javob);
 
-}
-run();
+// }
+// run();
 
 
 
